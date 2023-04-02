f46435230b75754ab7f10af8e7269cfaf2105f32
worasit <worasit.daimongkol@thoughtworks.com>
2023-04-02 15:36:15 +0700

    Implemented the Home page to enter session data.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,65 @@
 import {
+  IonButton,
   IonContent,
   IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
   IonPage,
   IonTitle,
   IonToolbar,
-} from '@ionic/react'
-import ExploreContainer from '../components/ExploreContainer'
-import './Home.css'
+} from "@ionic/react";
+import "./Home.css";
+import React, { useState } from "react";
+import { useVault } from "../hooks/useVault";
 
 const Home: React.FC = () => {
+  const { session, storeSession, restoreSession } = useVault();
+  const [data, setData] = useState<string>("");
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Identity Vault</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+
+      <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Identity Vault</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+
+        <IonList>
+          <IonItem>
+            <IonLabel position="floating">Enter the "session" data</IonLabel>
+            <IonInput
+              value={data}
+              onIonInput={(e) => {
+                console.log("enter session: " + e.detail.value!.toString());
+                setData(e.detail.value!.toString());
+              }}
+            />
+          </IonItem>
+          <IonItem>
+            <div style={{ flex: "auto" }}>
+              <IonButton expand="block" onClick={() => storeSession(data)}>
+                Set Session Data
+              </IonButton>
+            </div>
+          </IonItem>
+
+          <IonItem>
+            <IonLabel>
+              <div>Session Data: {session}</div>
+            </IonLabel>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
